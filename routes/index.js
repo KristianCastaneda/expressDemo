c124fda6273d8e1c38b91d8744023eb501144a7b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-03-10 14:01:43 +0000

    added ejs partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'expressDemo' });
+  res.render('index', { title: "expressDemo", firstHeadline: "Here's my first headline" });
 });
 
 module.exports = router;
